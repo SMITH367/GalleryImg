45b5beab9121631cdeb37f1fa418219d328a81ec
BrayanP <brayansmithpuente@gmail.com>
2021-05-12 00:59:15 -0500

    changes in  home page

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,30 @@
-console.clear(); 
+
+
+//Dinamic change of background in the hero
+
+const hero = document.getElementById("hero");
+
+let ramdom =  Math.floor(Math.random() * (4 - 1)) + 1;
+
+if(ramdom ===1){
+
+    hero.classList.add("hero1");
+
+} else if(ramdom ===2){
+
+    hero.classList.add("hero2");
+
+} else if(ramdom ===3){
+
+    hero.classList.add("hero3");
+
+}
+
+//side bar
+
+const toogle = document.querySelector(".toogle");
+
+toogle.addEventListener("click", function(){
+    document.getElementById("side-bar").classList.toggle("active"); 
+
+})
--- Js/sidebar.js
+++
@@ -1,9 +0,0 @@
-const toogle = document.querySelector(".toogle");
-
-toogle.addEventListener("click", function(){
-    document.getElementById("side-bar").classList.toggle("active"); 
-
-
-
-})
-
